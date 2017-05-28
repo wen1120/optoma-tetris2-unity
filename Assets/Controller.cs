dbd3c76481041d8aedfb41200ae690b3e4cc9a20
KUAN-YING CHOU <kuanyingchou@gmail.com>
2017-05-28 10:32:00 +0800

    add multi face

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,26 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour { // TODO: rename
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// GetComponent<Transform> ().Rotate (new Vector3(0, 1, 0));
-	}
+  public struct FaceParams {
+    public int id;
+    public float x;
+    public float y;
+    public float r;
 
-	void UpdateFacePosition(string position) {
-		string[] xy = position.Split (';');
-		float x = float.Parse (xy [0]);
-		float y = float.Parse (xy [1]);
-		float r = float.Parse (xy [2]);
+    public FaceParams(string args) {
+      string[] xy = args.Split (';');
+      id = int.Parse (xy [0]);
+      if (xy.Length == 4) {
+        x = float.Parse (xy [1]);
+        y = float.Parse (xy [2]);
+        r = float.Parse (xy [3]);
+      } else {
+        x = 0;
+        y = 0;
+        r = 0;
+      }
+    }
+  }
 
-		transform.position = new Vector3 (-(x - 0.5f) * 20, -(y - 0.5f) * 20, 0);
-		transform.rotation = Quaternion.AngleAxis (-r, Vector3.forward);
-	}
+  Dictionary<int, GameObject> faces = new Dictionary<int, GameObject>();
+
+  void FaceEnter(string args) {
+    var f = new FaceParams(args);
+    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    cube.transform.position = new Vector3 (-(f.x - 0.5f) * 20, -(f.y - 0.5f) * 20, 0);
+    cube.transform.rotation = Quaternion.AngleAxis (-f.r, Vector3.forward);
+    faces.Add(f.id, cube);
+  }
+
+  void FaceMove(string args) {
+    var f = new FaceParams(args);
+
+    if(faces.ContainsKey(f.id)) {
+      GameObject cube = faces[f.id];
+      cube.transform.position = new Vector3 (-(f.x - 0.5f) * 20, -(f.y - 0.5f) * 20, 0);
+      cube.transform.rotation = Quaternion.AngleAxis (-f.r, Vector3.forward);
+    }
+
+  }
+
+  void FaceExit(string args) {
+    var f = new FaceParams(args);
+    if(faces.ContainsKey(f.id)) {
+      GameObject cube = faces[f.id];
+      GameObject.Destroy(cube);
+      faces.Remove(f.id);
+    }
+  }
 }
---
+++ Assets/Editor/Exporter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class Exporter {
+	public static void export() {
+
+		string[] args = System.Environment.GetCommandLineArgs();
+		Debug.Log ("args: "+string.Join (", ", args));
+		string path = args [args.Length - 1];
+
+		BuildPlayerOptions options = new BuildPlayerOptions();
+
+		options.scenes = getScenes();
+		options.target = BuildTarget.Android;
+		options.locationPathName = path;
+		options.options = BuildOptions.AcceptExternalModificationsToPlayer;
+
+		BuildPipeline.BuildPlayer (options);
+	}
+
+	private static string[] getScenes() {
+		// from http://answers.unity3d.com/questions/10482/accessing-buildsettings-from-buildsettingsasset.html
+		return (from scene in EditorBuildSettings.scenes
+		        where scene.enabled
+		        select scene.path).ToArray ();
+	}
+}
